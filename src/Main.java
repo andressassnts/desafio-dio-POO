38c247ee6f993d40ed2a8bcc6b00ab8f63811b42
Andressa <andressa.ssnts@hotmail.com>
2022-07-19 20:48:57 -0300

    Mentoria e Curso Bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
+import java.util.Set;
 
 import java.time.LocalDate;
 
@@ -22,9 +25,32 @@ public class Main {
         mentoria.setDescricao("descrição da mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAndressa = new Dev();
+        devAndressa.setNome("Andressa");
+        devAndressa.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudo Inscritos" + devAndressa.getConteudosInscritos());
+        devAndressa.progredir();
+        System.out.println("Conteudo Inscritos" + devAndressa.getConteudosInscritos());
+        System.out.println("Conteudo Concluidos" + devAndressa.getConteudosConcluidos());
+
+        System.out.println("---------------------");
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudo Inscritos" + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("Conteudo Inscritos" + devCamila.getConteudosInscritos());
+        System.out.println("Conteudo Concluidos" + devCamila.getConteudosConcluidos());
+
+
 
 
 
